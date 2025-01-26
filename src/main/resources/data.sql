06f9b8f4cc670b9efc92d41b2491cc0a9e387257
sth3r <vrodrigues.esther@gmail.com>
2025-01-26 20:06:09 -0300

    foiii????aa

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,13 +17,14 @@
 
 # insert into token_confirmacao_email(id, data_de_criacao, token, usuario_id) value (1,'2024-05-02 10:00','aecc73b7-dae5-4011-925d-e07633d9993f',1);
 
-INSERT INTO anotacaos (id, titulo, conteudo, dia_id)
-VALUES (1, 'meu sonho de hoje', 'sonhei com elefantes e cobras', '1'),
-       (2, 'lista de comprsa', 'arroz sabao em po feijao cafe balinha azedinha', '2'),
-       (3, 'reuniao de hoje', 'chata demais lembrar de pedir emissao', '1'),
-       (4, 'receita de bolo', 'farinha açucar ovo leite margarina fermento e forno ta pronto', '2'),
-       (5, 'list de convidados', 'por enquanto ninguem', '1');
-
 INSERT INTO dias (id, data, usuario_id)
 VALUES (1, '2025-01-26', 1),
        (2, '2025-01-27', 1);
+
+INSERT INTO anotacaos (id, titulo, conteudo, dia_id)
+VALUES (1, 'meu sonho de hoje', 'sonhei com elefantes e cobras', 1),
+       (2, 'lista de comprsa', 'arroz sabao em po feijao cafe balinha azedinha', 2),
+       (3, 'reuniao de hoje', 'chata demais lembrar de pedir emissao', 1),
+       (4, 'receita de bolo', 'farinha açucar ovo leite margarina fermento e forno ta pronto', 2),
+       (5, 'list de convidados', 'por enquanto ninguem', 1);
+
